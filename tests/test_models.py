9a2291da23cdcaa9a332012e37bac07417c5b56a
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2022-01-21 12:21:49 +0000

    Moved patient tests into a separate file

--- tests/test_models.py
+++ tests/test_models.py
@@ -113,37 +113,4 @@ def test_patient_normalise(test, expected, raises):
         npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
 
 
-def test_doctor_is_person():
-    """Check if a doctor is a person."""
-    from inflammation.models import Doctor, Person
-    doc = Doctor("Sheila Wheels")
-    assert isinstance(doc, Person)
-
-
-def test_patient_is_person():
-    """Check if a patient is a person. """
-    from inflammation.models import Patient, Person
-    alice = Patient("Alice")
-    assert isinstance(alice, Person)
-
-
-def test_patients_added_correctly():
-    """Check patients are being added correctly by a doctor. """
-    from inflammation.models import Doctor, Patient
-    doc = Doctor("Sheila Wheels")
-    alice = Patient("Alice")
-    doc.add_patient(alice)
-    assert doc.patients is not None
-    assert len(doc.patients) == 1
-
-
-def test_no_duplicate_patients():
-    """Check adding the same patient to the same doctor twice does not result in duplicates. """
-    from inflammation.models import Doctor, Patient
-    doc = Doctor("Sheila Wheels")
-    alice = Patient("Alice")
-    doc.add_patient(alice)
-    doc.add_patient(alice)
-    assert len(doc.patients) == 1
-
 
